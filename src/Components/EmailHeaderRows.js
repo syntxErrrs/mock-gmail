8d9f9c17bf819dd8d1d8cbd8dfc74d31dd1f4f76
Jonathon Phillips <jonathan.phillips@austincc.edu>
2021-10-21 16:28:25 -0500

    wip search bar filteredEmails

--- src/Components/EmailHeaderRows.js
+++ src/Components/EmailHeaderRows.js
@@ -6,29 +6,53 @@ import ComposeForm from "./ComposeForm";
 
 const EmailHeaderRows = (props) => {
     const [emails, setEmails] = useState();
+    //TODO
+    const [filteredEmails, setFilteredEmails] = useState();
+    const [value, setValue] = useState('');
 
     useEffect(() => {
         const getResponse = async () => {
             const response = await axios.get('http://localhost:3001/emails');
             setEmails(response.data);
         }
-        getResponse().then(() => {});
+        getResponse().then(() => {
+        });
     }, [])
 
     const emailHeaderRows = !!emails ?
         emails?.map((email, index) => <EmailHeaderRow key={index} email={email} setFocusEmail={props.setFocusEmail}/>) :
         <></>;
 
+    const searchForFocusEmail = () => {
+        const newEmails = emails?.filter(email => email.subject.startsWith(value));
+        setEmails(newEmails);
+    }
+
     return props.compose ? (
-        <div className="ComposeMessageContainer"><ComposeForm /></div>
+        <div className="ComposeMessageContainer"><ComposeForm/></div>
     ) : (
         <div className="EmailHeaderContainer">
             {
                 !!props.focusEmail ?
-                    (<FocusEmailRow email={props.focusEmail} setFocusEmail={props.setFocusEmail} />) :
+                    (<FocusEmailRow email={props.focusEmail} setFocusEmail={props.setFocusEmail}/>) :
                     (
                         <>
-                            <button id="composeEmail" className="ComposeButton" onClick={() => props.setCompose(true)}>Compose</button>
+                            <button id="composeEmail" className="ComposeButton" onClick={() => props.setCompose(true)}>
+                                Compose
+                            </button>
+                            <label>
+                                Search By Subject
+                                <input
+                                    name="search"
+                                    placeholder="Search by subject..."
+                                    type="string"
+                                    value={value}
+                                    onChange={(e) => {
+                                        setValue(e.target.value);
+                                        searchForFocusEmail(value);
+                                    }}
+                                />
+                            </label>
                             {emailHeaderRows}
                         </>
                     )
